91f5f33087b577193f5c67ae7e9c35da149f6a9e
Pragti Singh <pragti.singh@sap.com>
2022-11-19 10:45:24 +0530

    Added files

---
+++ java_basic_examples/hello_area_description/src/main/java/com/projecttango/examples/java/helloareadescription/AddFile1.java
@@ -0,0 +1,19 @@
+package com.projecttango.examples.java.helloareadescription;
+
+/**
+ * Contains an ADF Name and its UUID.
+ */
+
+class AdfData {
+    public final String uuid;
+    public final String name;
+
+    public AdfData(String uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+    }
+}
+
+/**
+ * created new file for OSSE LAB 4.
+ */
